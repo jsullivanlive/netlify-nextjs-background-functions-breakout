ebccd42b63f4f2c79bac82dae9b71c5ec61be824
James Sullivan <jsullivan@cloudanswers.com>
2022-11-21 12:39:16 -0500

    fix lint error

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 type Data = {
   name: string;
