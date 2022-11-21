7994669b0f756f52467cbb823439b65814171cf9
James Sullivan <jsullivan@cloudanswers.com>
2022-11-21 13:00:10 -0500

    trying http post since maybe it's just logs that are missing??

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -11,8 +12,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  for (let i = 0; i <= 5; i++) {
+  const { url, headers } = req;
+
+  for (let i = 0; i <= 10; i++) {
     console.log(`sleeping ${i}`);
+    axios.post("https://en1ta4a0x9c6i.x.pipedream.net", { i, url, headers });
     await sleep(i * 1000);
   }
 
